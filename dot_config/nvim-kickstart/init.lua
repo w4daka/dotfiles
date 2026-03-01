2e18e0f31940c1897dacb30c9acd2b689b2b31fc
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-01 17:50:01 +0900

    skkeleton edit

--- dot_config/nvim-kickstart/init.lua
+++ dot_config/nvim-kickstart/init.lua
@@ -226,6 +226,43 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- 挿入モード (Insert mode) での Emacs ライクな操作
+local map = vim.keymap.set
+
+map('i', '<C-b>', '<Left>', { desc = '左へ移動' })
+map('i', '<C-f>', '<Right>', { desc = '右へ移動' })
+map('i', '<C-a>', '<ESC>I', { desc = '行頭へ移動' })
+map('i', '<C-e>', '<End>', { desc = '行末へ移動' })
+map('i', '<C-d>', '<Del>', { desc = '一文字削除' })
+
+vim.keymap.set({ 'n', 'x' }, 'x', '"_d', { desc = 'Delete using blackhole register' })
+vim.keymap.set('n', 'X', '"_D', { desc = 'Delete using blackhole register' })
+vim.keymap.set('o', 'x', 'd', { desc = 'Delete using x' })
+
+vim.keymap.set('c', '<c-b>', '<left>', { desc = 'Emacs like left' })
+vim.keymap.set('c', '<c-f>', '<right>', { desc = 'Emacs like right' })
+vim.keymap.set('c', '<c-a>', '<home>', { desc = 'Emacs like home' })
+vim.keymap.set('c', '<c-e>', '<end>', { desc = 'Emacs like end' })
+vim.keymap.set('c', '<c-h>', '<bs>', { desc = 'Emacs like bs' })
+vim.keymap.set('c', '<c-d>', '<del>', { desc = 'Emacs like del' })
+
+vim.keymap.set('n', '<space>;', '@:', { desc = 'Re-run the last command' })
+vim.keymap.set('n', '<space>w', '<cmd>write<cr>', { desc = 'Write' })
+vim.keymap.set('c', '<c-n>', function()
+  return vim.fn.wildmenumode() == 1 and '<c-n>' or '<down>'
+end, { expr = true, desc = 'Select next' })
+vim.keymap.set('c', '<c-p>', function()
+  return vim.fn.wildmenumode() == 1 and '<c-p>' or '<up>'
+end, { expr = true, desc = 'Select previous' })
+
+vim.keymap.set('n', '<space>q', function()
+  if not pcall(vim.cmd.tabclose) then
+    vim.cmd.quit()
+  end
+end, { desc = 'Quit current tab or window' })
+
+vim.keymap.set('n', 'q:', '<nop>', { desc = 'Disable cmdwin' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 

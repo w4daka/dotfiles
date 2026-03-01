aeee07b5a7e3aae0d78b2215dcc71311749f2986
w4daka <ignorenegativeideology999@gmail.com>
2026-03-01 17:50:01 +0900

    skkeleton edit

--- dot_config/nvim-kickstart/lua/custom/plugins/skkeleton.lua
+++ dot_config/nvim-kickstart/lua/custom/plugins/skkeleton.lua
@@ -9,7 +9,7 @@ return {
       local patch_global = vim.fn['ddc#custom#patch_global']
 
       patch_global('ui', 'pum')
-      patch_global('sources', { 'lsp', 'skkeleton', 'around' })
+      patch_global('sources', { 'lsp', 'around' })
 
       patch_global('sourceOptions', {
         ['_'] = {
@@ -20,20 +20,23 @@ return {
           mark = 'LSP',
           forceCompletionPattern = [[\.\w*|:\w*|->\w*]],
         },
-        skkeleton = {
-          mark = 'skk',
-          isVolatile = true,
-          minAutoCompleteLength = 1,
-        },
-        copilot = {
-          mark = 'Co',
-          matchers = { 'matcher_head' },
-          sorters = { 'sorter_rank' },
-          minAutoCompleteLength = 0,
-          isVolatile = true,
-          forceCompletionPattern = [[\w+]],
-          timeout = 2000,
-        },
+        -- skkeleton = {
+        --   mark = 'skk',
+        --   matchers = {}, -- matcher は必須
+        --   sorters = {},
+        --   isVolatile = true,
+        --   minAutoCompleteLength = 1,
+        --   forceCompletionPattern = [[\k+]],
+        -- },
+        -- copilot = {
+        --   mark = 'Co',
+        --   matchers = { 'matcher_head' },
+        --   sorters = { 'sorter_rank' },
+        --   minAutoCompleteLength = 0,
+        --   isVolatile = true,
+        --   forceCompletionPattern = [[\w+]],
+        --   timeout = 2000,
+        -- },
         around = {
           mark = 'A',
         },
@@ -81,26 +84,46 @@ return {
   { 'Shougo/ddc-filter-sorter_rank' },
   { 'Shougo/ddc-filter-matcher_head' },
 
-  -- 5. Skkeleton (元のまま)
+  -- 5. Skkeleton
   {
     'vim-skk/skkeleton',
-    lazy = false,
+    dependencies = {
+      'vim-denops/denops.vim',
+      'Shougo/ddc.vim',
+      'Shougo/ddc-ui-pum',
+    },
     config = function()
       vim.fn['skkeleton#config'] {
-        kanaTable = 'rom',
         globalDictionaries = { '~/.skk/SKK-JISYO.L' },
         eggLikeNewline = true,
-        keepState = true,
       }
-
       local opts = { noremap = true, silent = true }
       vim.keymap.set({ 'i', 'c', 't' }, '<C-j>', '<Plug>(skkeleton-toggle)', { noremap = false })
+
+      -- pum.vim との連携用マップ（現状通り）
       vim.keymap.set({ 'i', 'c' }, '<C-n>', '<cmd>call pum#map#insert_relative(+1)<CR>', opts)
       vim.keymap.set({ 'i', 'c' }, '<C-p>', '<cmd>call pum#map#insert_relative(-1)<CR>', opts)
       vim.keymap.set({ 'i', 'c' }, '<C-y>', '<cmd>call pum#map#confirm()<CR>', opts)
       vim.keymap.set({ 'i', 'c' }, '<C-e>', '<cmd>call pum#map#cancel()<CR>', opts)
+
+      vim.fn['ddc#custom#patch_global']('ui', 'pum')
+      vim.fn['ddc#custom#patch_global']('sources', { 'skkeleton' })
+      vim.fn['ddc#custom#patch_global'] {
+        sourceOptions = {
+          ['skkeleton'] = {
+            mark = 'skkeleton',
+            matchers = {},
+            sorters = {},
+            converters = {},
+            isVolatile = true,
+            minAutoCompleteLength = 1,
+          },
+        },
+      }
+      vim.fn['ddc#enable']()
     end,
   },
+
   {
     'delphinus/skkeleton_indicator.nvim',
     config = function()

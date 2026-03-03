e1815eae136ec174f8edeb71e16fa6fa4b80f166
w4daka <ignorenegativeideology999@gmail.com>
2026-03-03 16:47:55 +0900

    lazydev.nvim edit

--- dot_config/nvim-kickstart/lua/custom/plugins/lazydev.lua
+++ dot_config/nvim-kickstart/lua/custom/plugins/lazydev.lua
@@ -2,8 +2,8 @@ return {
   -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
   -- used for completion, annotations and signatures of Neovim apis
   'folke/lazydev.nvim',
-  event = 'VimEnter',
   ft = 'lua',
+  priority = 100, -- LSP よりも先に読み込ませる
   opts = {
     library = {
       -- Load luvit types when the `vim.uv` word is found

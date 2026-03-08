96dc6b785078203be74cfeb1e0adcc760f8c9c91
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-09 07:32:47 +0900

    markdown-preview Optimaize

--- dot_config/nvim/lua/custom/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/custom/plugins/markdown-preview.lua
@@ -1,16 +1,9 @@
+-- install without yarn or npm
 return {
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && yarn install',
-  init = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
-  end,
   ft = { 'markdown' },
-  config = function()
-    vim.g.mkdp_browser = 'chrome.exe'
-    vim.g.mkdp_auto_start = 0
-    vim.g.mkdp_auto_close = 1
-    vim.g.mkdp_refresh_slow = 0
-    vim.g.mkdp_open_to_the_world = 0
+  build = function()
+    vim.fn['mkdp#util#install']()
   end,
 }

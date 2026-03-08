127053843f10fa221a953d97af0f6bb944ace459
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-08 09:14:27 +0900

    wezterm.lua edit

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,32 @@
-
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
+
+-- 基本フォント + フォールバック
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font", -- 英数字・記号・Powerline
+	"Noto Sans CJK JP", -- 日本語
+})
+
+config.font_size = 14
+
+-- CJK文字だけ日本語フォントに置き換え（Nightly専用）
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrainsMono Nerd Font"),
+	},
+	{
+		intensity = "Normal",
+		italic = true, -- イタリック有効
+		font = wezterm.font("Noto Sans CJK JP"),
+	},
+}
+config.adjust_window_size_when_changing_font_size = false
 -- 背景の透過
 config.window_background_opacity = 0.8
 
@@ -22,13 +44,13 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.use_fancy_tab_bar = false
 
 config.window_frame = {
-  inactive_titlebar_bg = "none",
-  active_titlebar_bg = "none",
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
 }
 
 -- タブバーを背景色に合わせる
 config.window_background_gradient = {
-  colors = { "#000000" },
+	colors = { "#000000" },
 }
 
 -- タブの追加ボタンを非表示
@@ -36,9 +58,9 @@ config.show_new_tab_button_in_tab_bar = false
 
 -- タブ同士の境界線を非表示
 config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
 }
 
 -- タブの形をカスタマイズ
@@ -48,26 +70,26 @@ local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#FFFFFF"
-  local edge_background = "none"
-  if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#FFFFFF"
-  end
-  local edge_foreground = background
-  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
 end)
 
 return config

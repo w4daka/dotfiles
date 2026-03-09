8f1109a61ec5f78e61be4e1782a8bdc3bb264870
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-09 09:41:45 +0900

    Feature: npm ass

--- dot_config/i3/executable_touchpad.sh
+++ dot_config/i3/executable_touchpad.sh
@@ -4,6 +4,7 @@ DEVICE="VEN_04F3:00 04F3:32EB Touchpad"
 
 state=$(xinput list-props "$DEVICE" | grep "Device Enabled" | awk '{print $4}')
 
+# 初期状態は無効にしたい → ONならOFFに、OFFならONにしない
 if [ "$state" = "1" ]; then
     xinput disable "$DEVICE"
 else

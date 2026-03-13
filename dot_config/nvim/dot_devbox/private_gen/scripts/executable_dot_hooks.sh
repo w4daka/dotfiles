e6684cdcb1081c7b119ee3ced587eaedc09c7fe6
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-13 15:07:14 +0900

    feat(run_once_script): changed install script

--- dot_config/nvim/dot_devbox/private_gen/scripts/executable_dot_hooks.sh
+++
@@ -1,3 +0,0 @@
-test -z $DEVBOX_COREPACK_ENABLED || corepack enable --install-directory "/home/w4dkaka/.config/nvim/.devbox/virtenv/nodejs_20/corepack-bin/"
-test -z $DEVBOX_COREPACK_ENABLED || export PATH="/home/w4dkaka/.config/nvim/.devbox/virtenv/nodejs_20/corepack-bin/:$PATH"
-echo 'Welcome to devbox!' > /dev/null
--- dot_config/nvim/dot_devbox/private_gen/scripts/executable_test.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-if [ -z "$__DEVBOX_SKIP_INIT_HOOK_b64abef24954579f238b9d6a9e8c13c8bb3c9ce3ff580080e9bf36885e9ddbae" ]; then
-    . "/home/w4dkaka/.config/nvim/.devbox/gen/scripts/.hooks.sh"
-fi
-
-echo "Error: no test specified" && exit 1

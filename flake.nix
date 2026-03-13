ab101e22ede3774405beb84520e83f67f65d215b
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-13 14:41:35 +0900

    feat(home/home.nix-flake.nix): nix add

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+
+      homeConfigurations.w4dkaka = home-manager.lib.homeManagerConfiguration {
+
+        inherit pkgs;
+
+        modules = [
+          ./home/home.nix
+        ];
+      };
+
+    };
+}

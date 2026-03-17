7f9fb05610f92ea096f43ff069b38dcc13c89fd6
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-17 12:46:43 +0900

    feat(dot_config/nvim/lua/completion/skk.lua)skkeleton-azik add

--- dot_config/nvim/lua/completion/skk.lua
+++ dot_config/nvim/lua/completion/skk.lua
@@ -10,7 +10,7 @@ return {
     "vim-skk/skkeleton",
     dependencies = {
       "vim-denops/denops.vim",
-      "k16em/skkeleton-azik-kanatable",
+      "shg-eo/skkeleton-azik",
     },
 
     config = function()
@@ -29,72 +29,8 @@ return {
         userDictionary = "~/.config/skk/user_dict",
         completionRankFile = "~/.config/skk/rank.json",
       })
-      vim.fn["skkeleton#register_kanatable"]("azik", {
-
-        -- 基本
-        [";"] = { "ん" },
-
-        ["n"] = { "ん" },
-        ["nn"] = { "ん" },
-        ["n'"] = { "ん" },
-
-        ["l"] = { "っ" },
-        ["L"] = { "っ" },
-
-        ["-"] = { "ー" },
-        [":"] = { "ー" },
-
-        -- 小文字
-        ["xxa"] = { "ぁ" },
-        ["xxi"] = { "ぃ" },
-        ["xxu"] = { "ぅ" },
-        ["xxe"] = { "ぇ" },
-        ["xxo"] = { "ぉ" },
-
-        ["xya"] = { "ゃ" },
-        ["xyu"] = { "ゅ" },
-        ["xyo"] = { "ょ" },
-
-        ["xxwa"] = { "ゎ" },
-
-        -- AZIK系
-        ["kt"] = { "こと" },
-        ["sk"] = { "する" },
-        ["st"] = { "した" },
-        ["tt"] = { "った" },
-        ["tk"] = { "てき" },
-
-        -- 記号
-        ["z/"] = { "・" },
-        ["z "] = { "　" },
-
-        ["z~"] = { "～" },
-        ["z-"] = { "～" },
-
-        ["z."] = { "…" },
-
-        ["z:"] = { "：" },
-
-        ["z["] = { "『" },
-        ["z]"] = { "』" },
-      })
-
       -- IME toggle
       vim.keymap.set({ "i", "c", "t" }, "<C-j>", "<Plug>(skkeleton-toggle)")
-
-      -- モード
-      local fn = vim.fn
-      -- カタカナ
-      fn["skkeleton#register_keymap"]("input", "q", "katakana")
-
-      -- 半角カタカナ
-      fn["skkeleton#register_keymap"]("input", "Q", "hankatakana")
-
-      -- 全角英数
-      fn["skkeleton#register_keymap"]("input", "@", "zenkaku")
-      fn["skkeleton#register_keymap"]("input", "/", "abbrev")
-
-      vim.keymap.set("i", "'", "<Plug>(skkeleton-disable)")
     end,
   },
 }

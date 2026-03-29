b7be220d00879cfd3601387806ead50f19b39659
w4daka <213398204+w4daka@users.noreply.github.com>
2026-03-29 21:34:28 +0900

    feat(image.lua)image.nvim add

--- dot_config/nvim/lua/languages/markdown.lua
+++ dot_config/nvim/lua/languages/markdown.lua
@@ -20,6 +20,11 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" }, -- if you use standalone mini plugins
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      render_modes = { "n", "c" },
+      heading = { enabled = false },
+      link = { enabled = false },
+      pipe_table = { enabled = false },
+    },
   },
 }

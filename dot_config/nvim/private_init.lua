dcadfce93130f7ad19b02568996cb5af39b9edab
w4daka <w4daka999@gmail.com>
2026-04-02 16:41:05 +0900

    feat devcontainer用のtmplをいろいろ追加

--- dot_config/nvim/private_init.lua
+++ dot_config/nvim/private_init.lua
@@ -23,24 +23,24 @@ vim.g.have_nerd_font = true
 
 -- https://scrapbox.io/vim-jp/boolean%E3%81%AA%E5%80%A4%E3%82%92%E8%BF%94%E3%81%99vim.fn%E3%81%AEwrapper_function
 vim.bool_fn = setmetatable({}, {
-  __index = function(_, key)
-    return function(...)
-      local v = vim.fn[key](...)
-      if not v or v == 0 or v == "" then
-        return false
-      elseif type(v) == "table" and next(v) == nil then
-        return false
-      end
-      return true
-    end
-  end,
+	__index = function(_, key)
+		return function(...)
+			local v = vim.fn[key](...)
+			if not v or v == 0 or v == "" then
+				return false
+			elseif type(v) == "table" and next(v) == nil then
+				return false
+			end
+			return true
+		end
+	end,
 })
 
 -- mermaid を有効化（デフォルトで true のことが多いが念のため）
 vim.g.mkdp_preview_options = {
-  mmarkdown = 1,
-  description_container = 1,
-  mermaid = 1, -- これ！
+	mmarkdown = 1,
+	description_container = 1,
+	mermaid = 1, -- これ！
 }
 -- example:
 -- if vim.bool_fn.has('mac') then ... end
@@ -53,12 +53,11 @@ vim.env.RUSTUP_AUTO_INSTALL = "0"
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out =
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 
 ---@type vim.Option
@@ -77,36 +76,36 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
-  { import = "lsp" },
-  { import = "completion" },
-  { import = "utilites" },
-  { import = "ai" },
-  { import = "languages" },
+	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
+	{ import = "lsp" },
+	{ import = "completion" },
+	{ import = "utilites" },
+	{ import = "ai" },
+	{ import = "languages" },
 }, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "🌙",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-      lazy = "💤 ",
-    },
-  },
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
 
 -- init.lua
 require("luasnip.loaders.from_lua").load({
-  paths = "~/.config/nvim/lua/snippets",
+	paths = "~/.config/nvim/lua/snippets",
 })

38a18cc83574861fbc4752d0d10f206a3cddab35
w4daka <213398204+w4daka@users.noreply.github.com>
2026-04-16 14:08:45 +0900

    feat(nvim/lua/utilites/private_help.lua)nvimのhelpを日本語にした

---
+++ dot_config/nvim/lua/utilites/private_help.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "vim-jp/vimdoc-ja",
+
+    lazy = true,
+    event = "VeryLazy",
+  },
+
+  {
+    "vim-jp/nvimdoc-ja",
+
+    -- 任意で遅延読み込み
+    keys = { "<F1>", "<Help>" },
+    event = "CmdlineEnter",
+  },
+}

ba5bc4e24f83d8aa151454415102b44a6f71e731
w4daka <213398204+w4daka@users.noreply.github.com>
2026-06-07 05:40:10 +0900

    feat deno_ls-lsp server add

--- dot_config/nvim/lua/core/private_lsp_servers.lua
+++ dot_config/nvim/lua/core/private_lsp_servers.lua
@@ -5,6 +5,15 @@ if status then
   capabilities = vim.tbl_deep_extend("force", capabilities, ddc_lsp.make_client_capabilities())
 end
 
+local group = vim.api.nvim_create_augroup("kyoh86-plug-denols-deno-docs", { clear = true })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = group,
+  pattern = { "deno:/*" },
+  callback = function()
+    vim.bo.bufhidden = "wipe"
+  end,
+})
+
 -- 2. サーバー設定の定義
 local servers = {
   lua_ls = {
@@ -58,6 +67,42 @@ local servers = {
   ruff = {},
   ocamllsp = {},
   ts_ls = {},
+  denols = {
+    settings = {
+      lint = true,
+      unstable = false,
+      suggest = {
+        completeFunctionCalls = true,
+        names = true,
+        paths = true,
+        autoImports = true,
+        imports = {
+          autoDiscover = true,
+          hosts = vim.empty_dict(),
+        },
+      },
+    },
+    single_file_support = false,
+    ---@param bufnr number
+    ---@param callback fun(root_dir?: string)
+    root_dir = function(bufnr, callback)
+      local path = vim.fn.fnamemodify(vim.fn.bufname(bufnr), ":p:h")
+      local marker = require("climbdir.marker")
+      local found = require("climbdir").climb(
+        path,
+        marker.one_of(
+          marker.has_readable_file("deno.json"),
+          marker.has_readable_file("deno.jsonc"),
+          marker.has_directory("denops")
+        ),
+        {}
+      )
+      if found then
+        vim.b[vim.fn.bufnr()].deno_deps_candidate = found .. "/deps.ts"
+        callback(found)
+      end
+    end,
+  },
   clangd = {},
   nixd = {
     settings = {

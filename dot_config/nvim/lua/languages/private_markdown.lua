6f42deeac3cf65de933cb27549a20dc262a816a6
w4daka <213398204+w4daka@users.noreply.github.com>
2026-08-16 14:37:49 +0900

    fix(dot_config/nvim/lua/languages/private_markdown.lua b/dot_config/nvim/lua/languages/private_markdown.lua) dependencies fieldの上書きを消した

--- dot_config/nvim/lua/languages/private_markdown.lua
+++ dot_config/nvim/lua/languages/private_markdown.lua
@@ -17,7 +17,6 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
